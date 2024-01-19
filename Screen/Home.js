6b9a3ebbf6ae8ab3eea10cec8191125af3a1a067
unknown <parita.reeranon@gmail.com>
2024-01-19 16:13:24 +0700

    Update login Screen

---
+++ Screen/Home.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image  } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+
+const Home = () => {
+    return (
+      <View style={styles.container}>
+        <Text>
+            Hello Welcome to Home Screen
+        </Text>
+      </View>
+    );
+  }
+
+  const styles =StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EAE1F1',
+        justifyContent: 'center', // Center vertically
+        alignItems: 'center', // Center horizontally
+      },
+  })
+export default Home;
