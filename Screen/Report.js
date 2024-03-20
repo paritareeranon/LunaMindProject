95ef871ef912c1831d928de239d2394bc15ad73f
unknown <Piyatida.chlm@gmail.com>
2024-03-20 15:22:53 +0700

    Report

--- Screen/Report.js
+++ Screen/Report.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, StyleSheet, Image, Button, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView, } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import Barchart from './Barchart';
+import { ProgressBar } from 'react-native-paper';
+import { BarChart, LineChart } from 'react-native-gifted-charts';
 
 const Report = () => {
 
@@ -18,6 +18,143 @@ const Report = () => {
     const handleOptionPress = (option) => {
         setSelectedOption(option);
     };
+    const barData = [
+        { value: 4, label: 'S', frontColor: '#B7E48A' },
+        { value: 1, label: 'M', frontColor: '#FD4F4F' },
+        { value: 5, label: 'T', frontColor: '#1ACB8B' },
+        { value: 3, label: 'W', frontColor: '#FFE459' },
+        { value: 5, label: 'T', frontColor: '#1ACB8B' },
+        { value: 1, label: 'F', frontColor: '#FD4F4F' },
+        { value: 2, label: 'S', frontColor: '#F78F55' },
+    ];
+
+    const frontColor = (value) => {
+        switch (value) {
+            case 5:
+                return '#1ACB8B';
+            case 4:
+                return '#B7E48A';
+            case 3:
+                return '#FFE459';
+            case 2:
+                return '#F78F55';
+            case 1:
+                return '#FD4F4F';
+            default:
+                return '#000000';
+        }
+    };
+    const linedata = [
+        { value: 4, label: '1/3' },
+        { value: 1, label: '2/3' },
+        { value: 5, label: '3/3' },
+        { value: 3, label: '4/3' },
+        { value: 5, label: '5/3' },
+        { value: 1, label: '6/3' },
+        { value: 2, label: '7/3' },
+        { value: 4, label: '8/3' },
+        { value: 1, label: '9/3' },
+        { value: 5, label: '10/3' },
+        { value: 3, label: '11/3' },
+        { value: 5, label: '12/3' },
+        { value: 1, label: '13/3' },
+        { value: 2, label: '14/3' },
+    ];
+    const stackData = [
+        {
+            stacks: [
+                { value: 10, color: '#1ACB8B' },
+                { value: 15, color: '#B7E48A', marginBottom: 2 },
+                { value: 10, color: '#FD4F4F', marginBottom: 2 },
+                { value: 14, color: '#1ACB8B', marginBottom: 2 },
+                { value: 20, color: '#F78F55' },
+            ],
+            label: 'Jan',
+        },
+        {
+            stacks: [
+                { value: 10, color: '#4ABFF4' },
+                { value: 11, color: 'orange', marginBottom: 2 },
+                { value: 15, color: '#28B2B3', marginBottom: 2 },
+            ],
+            label: 'Feb',
+        },
+        {
+            stacks: [
+                { value: 14, color: 'orange' },
+                { value: 18, color: '#4ABFF4', marginBottom: 2 },
+            ],
+            label: 'Mar',
+        },
+        {
+            stacks: [
+                { value: 7, color: '#4ABFF4' },
+                { value: 11, color: 'orange', marginBottom: 2 },
+                { value: 10, color: '#28B2B3', marginBottom: 2 },
+            ],
+            label: 'Apr',
+        },
+        {
+            stacks: [
+                { value: 10, color: 'orange' },
+                { value: 20, color: '#4ABFF4', marginBottom: 2 },
+            ],
+            label: 'May',
+        },
+        {
+            stacks: [
+                { value: 10, color: '#4ABFF4' },
+                { value: 11, color: 'orange', marginBottom: 2 },
+                { value: 15, color: '#28B2B3', marginBottom: 2 },
+            ],
+            label: 'Jun',
+        },
+        {
+            stacks: [
+                { value: 14, color: 'orange' },
+                { value: 18, color: '#4ABFF4', marginBottom: 2 },
+            ],
+            label: 'Jul',
+        },
+        {
+            stacks: [
+                { value: 7, color: '#4ABFF4' },
+                { value: 11, color: 'orange', marginBottom: 2 },
+                { value: 10, color: '#28B2B3', marginBottom: 2 },
+            ],
+            label: 'Aug',
+        },
+        {
+            stacks: [
+                { value: 10, color: 'orange' },
+                { value: 20, color: '#4ABFF4', marginBottom: 2 },
+            ],
+            label: 'Sab',
+        },
+        {
+            stacks: [
+                { value: 10, color: '#4ABFF4' },
+                { value: 11, color: 'orange', marginBottom: 2 },
+                { value: 15, color: '#28B2B3', marginBottom: 2 },
+            ],
+            label: 'Oct',
+        },
+        {
+            stacks: [
+                { value: 14, color: 'orange' },
+                { value: 18, color: '#4ABFF4', marginBottom: 2 },
+            ],
+            label: 'Nov',
+        },
+        {
+            stacks: [
+                { value: 7, color: '#4ABFF4' },
+                { value: 11, color: 'orange', marginBottom: 2 },
+                { value: 10, color: '#28B2B3', marginBottom: 2 },
+            ],
+            label: 'Dec',
+        },
+    ];
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -44,33 +181,102 @@ const Report = () => {
                 </TouchableOpacity>
             </View>
             <ScrollView>
-                {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>My App</Text>
-                    <Barchart />
-                </View> */}
-                {/* <View style={styles.chart}></View> */}
-
-                <Text style={styles.Emotions}> Mood Bar </Text>
-                <View style={styles.moodrow}>
-                    <Image source={require('../img/5.png')} style={styles.mood} />
-                    <Image source={require('../img/4.png')} style={styles.mood} />
-                    <Image source={require('../img/3.png')} style={styles.mood} />
-                    <Image source={require('../img/2.png')} style={styles.mood} />
-                    <Image source={require('../img/1.png')} style={styles.mood} />
-                </View>
-                <View style={styles.textrow}>
-                    <Text style={styles.textmood}>55%</Text>
-                    <Text style={styles.textmood}>20%</Text>
-                    <Text style={styles.textmood}>40%</Text>
-                    <Text style={styles.textmood}>60%</Text>
-                    <Text style={styles.textmood}>12%</Text>
-                </View>
-                <Text style={styles.mens}> Menstruation </Text>
-                <View style={styles.row}>
-                    <Text style={styles.marker}> * </Text>
-                    <Text style={styles.menstext}> From 3 January to 5 January </Text>
-                </View>
-
+                {selectedOption === 'Weekly' && (
+                    <View>
+                        <View style={styles.card}>
+                            <BarChart
+                                barWidth={22}
+                                noOfSections={5}
+                                barBorderRadius={7}
+                                frontColor={frontColor}
+                                data={barData}
+                                yAxisThickness={0}
+                                xAxisThickness={0}
+                                maxValue={5}
+                                spacing={15}
+                            />
+                        </View>
+                        <Text style={styles.moodlevel}> Mood Level </Text>
+                        <View style={styles.moodrow}>
+                            <Image source={require('../img/5.png')} style={styles.mood} />
+                            <Image source={require('../img/4.png')} style={styles.mood} />
+                            <Image source={require('../img/3.png')} style={styles.mood} />
+                            <Image source={require('../img/2.png')} style={styles.mood} />
+                            <Image source={require('../img/1.png')} style={styles.mood} />
+                        </View>
+                        <View style={styles.textrow}>
+                            <Text style={styles.textLevel}>5</Text>
+                            <Text style={styles.textLevel}>4</Text>
+                            <Text style={styles.textLevel}>3</Text>
+                            <Text style={styles.textLevel}>2</Text>
+                            <Text style={styles.textLevel}>1</Text>
+                        </View>
+                    </View>
+                )}
+                {selectedOption === 'Monthly' && (
+                    <View>
+                        <View style={styles.card}>
+                            <Text style={styles.Emotions}> Mood Bar </Text>
+                            <View style={styles.moodrow}>
+                                <Image source={require('../img/5.png')} style={styles.mood} />
+                                <Image source={require('../img/4.png')} style={styles.mood} />
+                                <Image source={require('../img/3.png')} style={styles.mood} />
+                                <Image source={require('../img/2.png')} style={styles.mood} />
+                                <Image source={require('../img/1.png')} style={styles.mood} />
+                            </View>
+                            <View style={styles.textrow}>
+                                <Text style={styles.textmood}>20%</Text>
+                                <Text style={styles.textmood}>20%</Text>
+                                <Text style={styles.textmood}>20%</Text>
+                                <Text style={styles.textmood}>20%</Text>
+                                <Text style={styles.textmood}>20%</Text>
+                            </View>
+                            <ProgressBar
+                                style={styles.progress}
+                                progress={0.2}
+                                color="#1ACB8B"
+                            />
+                        </View>
+                        <Text style={styles.mens}> Menstruation </Text>
+                        <View style={styles.row}>
+                            <Text style={styles.marker}> * </Text>
+                            <Text style={styles.menstext}> From 3 January to 5 January </Text>
+                        </View>
+                        <View style={styles.card}>
+                            <Text style={styles.Emotions}> Mood Flow </Text>
+                            <LineChart
+                                height={200}
+                                width={250}
+                                data={linedata}
+                                color={'#D6D6D6'}
+                                thickness={4}
+                                noOfSections={5}
+                                dataPointsColor={'#3F3C3C'}
+                                maxValue={5}
+                                yAxisThickness={0}
+                                xAxisThickness={0}
+                            />
+                        </View>
+                       
+                    </View>
+                )}
+                {selectedOption === 'Yearly' && (
+                    <View style={styles.cardYearly}>
+                        <Text style={styles.Emotions}> Mood Flow </Text>
+                        <BarChart
+                            width={250}
+                            height={400}
+                            rotateLabel
+                            barWidth={18}
+                            spacing={25}
+                            noOfSections={4}
+                            barBorderRadius={6}
+                            stackData={stackData}
+                            yAxisThickness={0}
+                                xAxisThickness={0}
+                        />
+                    </View>
+                )}
             </ScrollView >
         </View >
     )
@@ -79,6 +285,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+
     },
     header: {
         flexDirection: 'row',
@@ -138,8 +345,15 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#3F3C3C',
         fontWeight: 'bold',
-        marginTop: '10%',
-        paddingLeft: '10%',
+        paddingLeft: 12,
+        marginBottom: 20,
+    },
+    moodlevel: {
+        fontSize: 16,
+        color: '#3F3C3C',
+        fontWeight: 'bold',
+        paddingLeft: 30,
+        marginTop: 60,
     },
     row: {
         flexDirection: 'row',
@@ -156,7 +370,7 @@ const styles = StyleSheet.create({
         height: 45,
         // margin: '2%',
         marginTop: '7%',
-        marginHorizontal: 8, 
+        marginHorizontal: 8,
     },
     textrow: {
         flexDirection: 'row',
@@ -165,12 +379,41 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     textmood: {
-        // margin: 27,
-        marginTop: 10,
         fontSize: 14,
-        // fontWeight: 'bold',
-        marginHorizontal: 15, 
-        backgroundColor: 'grey',
+        marginHorizontal: 15,
+        borderRadius: 5,
+    },
+    textLevel: {
+        fontSize: 14,
+        marginHorizontal: 27,
+        borderRadius: 5,
+    },
+    progress: {
+        width: '90%',
+        height: 40,
+        marginTop: 30,
+        alignSelf: 'center',
+        borderRadius: 15,
+        marginBottom: 15,
+    },
+    card: {
+        borderRadius: 25,
+        backgroundColor: '#F2F2F2',
+        width: '90%',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginTop: 20,
+        padding: 20,
+    },
+    cardYearly: {
+        borderRadius: 25,
+        backgroundColor: '#F2F2F2',
+        width: '90%',
+        height: '100%',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginTop: 20,
+        padding: 20,
     },
     mens: {
         fontSize: 16,
@@ -192,7 +435,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: '4%',
         paddingLeft: '13%',
-    }
+    },
 
 });
 export default Report;
